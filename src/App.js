ed9f0ae3db23c1c8b344b30fb7da66773a6df15f
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-21 13:10:04 -0400

    ready to publish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import dotenv from 'dotenv'; 
+
 import './App.css';
 
+dotenv.config()
+const process_api_key = process.env.REACT_APP_AIRTABLE_API_KEY;
+const airtable_api_url = process.env.REACT_APP_AIRTABLE_API_URL;
+const airtable_table = process.env.REACT_APP_AIRTABLE_TABLE;
+const airtable_base = process.env.REACT_APP_AIRTABLE_BASE; 
+
 function App() {
+
+  const [airtableData, setAirtableData] = useState([]);
+  const [Notes, setNotes] = useState('');
+  const [URL, setURL] = useState('');
+  const [Thumbnail, setThumbnail] = useState('');
+
+  useEffect(()=>{
+    fetch(`${airtable_api_url}/${airtable_base}/${airtable_table}`, {
+       "headers": {"Authorization": `Bearer ${process_api_key}`}
+    })
+    .then(res => res.json())
+    .then((data) => {
+      setAirtableData(data.records)
+    })
+  },[])
+
+
+  function showDetails(e) {
+    e.preventDefault();
+    const elementKey = e.target.id;
+    setNotes(airtableData[elementKey].fields.Notes)
+    setURL(airtableData[elementKey].fields.URL)
+    setThumbnail(airtableData[elementKey].fields.Attachments ? airtableData[elementKey].fields.Attachments[0].thumbnails.large.url : null)
+  }
+
+  function hideDetails() {
+    setNotes('')
+    setURL('')
+    setThumbnail('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='title'>journal</div>
+      <div className='subtitle'>during some strange times / best viewed on desktop</div>
+      <main> 
+
+        <div className='details'>
+          {Thumbnail && <div className='image'><img src={Thumbnail} width='50%' alt='thumbnail' /></div> }
+          <div className='notes'>{Notes}</div>
+          <div className='URL'><a href={URL} target='_blank' rel="noopener noreferrer">{URL}</a></div>
+        </div> 
+
+        <div className='entries'>
+          { airtableData !== undefined && 
+            airtableData.map( (row, i) => (
+              <div className='row' key={i}> 
+                <div className='name' id={i} onClick={(e) => showDetails(e)} >{row.fields.Name} | {row.createdTime.slice(5, 10)}</div>
+              </div>
+            ))
+          }
+        </div>
+        </main> 
+        <div className='credits'>Made by <a href="https://kalli-retzepi.com/" target='_blank' rel="noopener noreferrer" >Kalli</a> during her time at the <a href="https://www.recurse.com/" target='_blank' rel="noopener noreferrer">Recurse Center</a>.</div>    
     </div>
   );
 }
